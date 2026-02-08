ea3a0a9b5c16b0ad7624507647ae892a243d9b31
ivangegovdve-sudo <ivangegov.dve@gmail.com>
2026-02-08 12:54:02 +0200

    Fix CVDataProvider mounting at app root

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
+import { CVDataProvider } from "@/contexts/CVDataContext";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <CVDataProvider>
+    <App />
+  </CVDataProvider>
+);
