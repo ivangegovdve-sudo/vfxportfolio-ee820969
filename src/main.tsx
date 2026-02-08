a5b4f3e39f4ed4fbf9eef56a3187e3505da4544e
ivangegovdve-sudo <ivangegov.dve@gmail.com>
2026-02-08 13:03:43 +0200

    Reinforce CVDataProvider at true React render root

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,16 @@
+import { CVDataProvider } from "@/contexts/CVDataContext";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("React root element '#root' not found");
+}
+
+createRoot(rootElement).render(
+  <CVDataProvider>
+    <App />
+  </CVDataProvider>
+);
